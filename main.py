083c5809e18a4b1f1f639fc32556ee75e17f3376
ilwoong <alltun100@g.skku.edu>
2023-03-09 19:33:15 +0900

    twoSum function modified

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ def twoSum(nums: List[int], target: int) -> List[int]:
     for i , num in enumerate(nums):
         leftnum = target - num
         if leftnum in cand:
-            return([nums.index(leftnum), nums.index(num)])
+            print([nums.index(leftnum), nums.index(num)])
+            return
         cand.append(num)
 
 
 if __name__ == "__main__":
     nums = list(map(int, input("nums = ").split()))
     target = int(input("target = "))
-    output = twoSum( nums,target)
-    print(f"Output: {output}")
+    twoSum( nums,target)
